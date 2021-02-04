5c45c433b6c102838d1b79cbf55f080fbc8e1733
DSroD <dsrod@seznam.cz>
2021-02-04 20:01:13 +0100

    Add login, fix bugs, change match model

--- duckgame/tournament/models.py
+++ duckgame/tournament/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 class Player(models.Model):
     name = models.CharField(max_length=100)
-    wins = models.IntegerField(default=0)
-    looses = models.IntegerField(default=0)
     total_points = models.IntegerField(default=0)
     player_info = models.CharField(max_length=2000, default="No info provided.")
     def __str__(self):
@@ -15,7 +13,8 @@ class Match(models.Model):
     play_date = models.DateTimeField('match datetime')
     players = models.ManyToManyField(Player, related_name='matches')
     finished = models.BooleanField('finished', default=False)
-    winner_id = models.IntegerField(blank=True, null=True)
+    winners = models.ManyToManyField(Player, related_name='won_matches')
+    loosers = models.ManyToManyField(Player, related_name='lost_matches')
     
     def __str__(self):
         return " vs. ".join([player.name for player in self.players.all()])
