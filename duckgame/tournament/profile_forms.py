58f3476ce142acef1eb67e9b27a17c0502e8890d
DSroD <dsrod@seznam.cz>
2021-02-04 22:07:38 +0100

    add diacritics

--- duckgame/tournament/profile_forms.py
+++ duckgame/tournament/profile_forms.py
@@ -5,4 +5,4 @@ class GameAccountCreateForm(forms.Form):
     game_nick = forms.CharField(min_length=3, max_length=17, validators=[RegexValidator('^[a-zA-Z0-9_\\-]*$'),])
 
 class GameAccountProfileInfoUpdateForm(forms.Form):
-    profile_text = forms.CharField(min_length=0, max_length=2000, validators=[RegexValidator('^[a-zA-Z0-9_\\- .,&@;\\*\\+]*$')])
+    profile_text = forms.CharField(min_length=0, max_length=2000, validators=[RegexValidator('^[a-zA-Z0-9_\\- .,&@;\\*\\+À-ž]*$')])
